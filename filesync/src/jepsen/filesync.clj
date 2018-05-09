013533cf2753e0c1b0aec0c4c83a1638cd61f786
Thomas Eidens <thomas.eidens@tum.de>
2018-05-09 20:03:39 +0200

    Followed tutorial until chapter 4
    * Implemented reads & writes
    * added model & checker

--- filesync/src/jepsen/filesync.clj
+++ filesync/src/jepsen/filesync.clj
@@ -2,16 +2,20 @@
   (:require [clojure.tools.logging :refer :all]
             [clojure.string :as str]
             [jepsen
+             [checker :as checker]
              [cli :as cli]
              [control :as c]
              [client :as client]
              [db :as db]
              [generator :as gen]
              [tests :as tests]]
+            [jepsen.checker.timeline :as timeline]
             [jepsen.control.util :as cu]
-            [jepsen.os.debian :as debian]))
+            [jepsen.os.debian :as debian]
+            [knossos.model :as model]))
 
 (def dir "/csync")
+(def test-file (str dir "/test"))
 (def pidfile "/csync.pid")
 (def logfile "/csync.log")
 (def binary "/filesync/csync")
@@ -25,33 +29,30 @@
   (reify db/DB
     (setup! [_ test node]
       (info node "installing csync" version)
-      (Thread/sleep 1000)
-      (c/su (c/exec :mkdir dir))
-      (cu/start-daemon!
-       {:logfile logfile
-        :pidfile pidfile
-        :chdir dir}
-       binary
-       (case node
-         nodename-client :--client
-         nodename-server :--server)
-       :--host hostname-server
-       :--port 22
-       :--dir dir)
+      (c/su (c/exec :mkdir :-p dir))
 
-      (Thread/sleep 1000))
+      (c/on nodename-server
+            (c/su (c/exec :mkdir :-p dir))
+            (cu/start-daemon!
+             {:logfile logfile
+              :pidfile pidfile
+              :chdir dir}
+             binary
+             :--server
+             :--port 22
+             :--dir dir)
+            (c/exec :touch test-file)))
 
     (teardown! [_ test node]
       (info node "tearing down csync client")
-      (cu/stop-daemon! binary pidfile)
-     ;(c/su (c/exec :rm :-rf dir))
-      )
+      (c/su (c/exec :rm :-rf dir)))
 
     db/LogFiles
     (log-files [_ test node]
             [logfile])))
 
-(defn w [_ _] {:type :invoke, :f :add-file, :value (rand-int 20)})
+(defn w [_ _] {:type :invoke, :f :write, :value (rand-int 20)})
+(defn r [_ _] {:type :invoke, :f :read, :value nil})
 
 (defrecord Client []
   client/Client
@@ -60,15 +61,23 @@
 
   (setup! [this test])
 
-  (invoke! [this test op]
+  (invoke! [_ test op]
     (let [val (:value op)]
       (case (:f op)
-        :add-file (do (c/on nodename-client (c/exec :echo val :> (str dir "/" val)))
-                      (assoc op :type, :ok)))))
+        :write (do (c/on nodename-client
+                         ; write value to file on the client
+                         (c/exec :echo val :> test-file)
+                         (c/exec binary :--client :--host hostname-server
+                                 :--port 22 :--dir dir))
+                      (assoc op :type :ok))
+        :read (assoc op :type :ok,
+                     :value (c/on nodename-server
+                                  (c/exec :cat test-file)))
+        )))
 
   (teardown! [this test])
 
-    (close! [_ test]))
+  (close! [_ test]))
 
 (defn filesync-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
@@ -80,10 +89,15 @@
           :os debian/os
           :db (db"v0.1.0")
           :client (Client.)
-          :generator (->> w
+          :model (model/register)
+          :checker (checker/compose
+                    {:perf      (checker/perf)
+                     :linear    (checker/linearizable)
+                     :timeline  (timeline/html)})
+          :generator (->> (gen/mix [r w])
                           (gen/stagger 1)
                           (gen/nemesis nil)
-                          (gen/time-limit 2))}))
+                          (gen/time-limit 5))}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web
